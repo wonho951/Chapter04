6801c62433f212f629b736bf781197be17311c01
wonho <wonho951@gmail.com>
2021-06-14 15:55:30 +0900

    2021.06.14 컬렉션 프레임워크

--- src/com/javaex/ex02/MyList.java
+++ src/com/javaex/ex02/MyList.java
@@ -2,4 +2,57 @@ package com.javaex.ex02;
 
 public class MyList {
 
+	
+	//필드
+	private Object[] oArray;
+	private int crtPos;
+
+	
+	//생성자
+	public MyList() {
+		this.oArray = new Object[3];
+		crtPos = 0;		
+	}
+	
+	
+	//메소드 g/s
+	
+	//메소드 일반
+	
+	public void add(Object object) {
+		//배열의 크기가변 로직생략
+		oArray[crtPos] = object;
+		crtPos = crtPos++;
+	}
+	
+	public int size() {
+		return crtPos;
+	}
+	
+	public Object get(int index) {
+		return oArray[index];
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/com/javaex/ex02/MyListApp.java
+++ src/com/javaex/ex02/MyListApp.java
@@ -4,15 +4,28 @@ public class MyListApp {
 
 	public static void main(String[] args) {
 
-		
+		System.out.println("point관리------------------");
 		MyList pList= new MyList();	//	메모리에 올림
 		
-		Point p01 = new Point(1, 1);
-		Point p02 = new Point(2, 2);
+		Point p00 = new Point(1, 1);
+		Point p01 = new Point(2, 2);
+		Point p01 = new Point(3, 3);
 		
 		
+		pList.add(p00);
+		pList.add(p01);
+		pList.add(p02);
 		
+		for(int i = 0; i < pList.size(); i++) {
+			System.out.println(pList.get(i));
+		}
 		
+		System.out.println("circle관리------------------");
+		MyList cList = new MyList();
+		
+		Circle c00 = new Circle(5);
+		Circle c01 = new Circle(10);
+		Circle c02 = new Circle(15);
 		
 		
 		
