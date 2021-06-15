6d1672bd9add7c4af4e2672d1d2ad29e55f523f5
wonho <wonho951@gmail.com>
2021-06-15 12:39:38 +0900

    2021.06.15 수업내용 List

--- src/com/javaex/ex04/PointApp.java
+++ src/com/javaex/ex04/PointApp.java
@@ -8,7 +8,20 @@ public class PointApp {
 
 		ArrayList<Point> pList= new ArrayList<Point>();	//Point만 들어오게함.
 		
-		Point p01 = new Point(2, 2);
+		Point p01 = new Point(2, 4);
+		Point p02 = new Point(3, 6);
+		
+		pList.add(p01);
+		System.out.println(pList.size());
+		
+		
+		pList.add(p02);	//위치 안정해주면 알아서 끝에 들어감
+		System.out.println(pList.size());
+		
+		
+		Point p = pList.get(0);
+		
+		
 		
 		
 		
