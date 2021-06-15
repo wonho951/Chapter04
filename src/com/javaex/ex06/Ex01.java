186c51b300f00c16b611a5d8a6e113d0c93e707d
wonho <wonho951@gmail.com>
2021-06-15 15:46:05 +0900

    2021.06.15 수업내용 set

---
+++ src/com/javaex/ex06/Ex01.java
@@ -0,0 +1,26 @@
+package com.javaex.ex06;
+
+import java.util.HashSet;
+
+public class Ex01 {
+	
+	public static void main(String[]args) {
+		
+		//set --> 데이터 보관용
+		
+		//HashSet만든다
+		//HashSet iSet = new HashSet(); --> 원래 알고 있는 데이터 올리는 방식
+		//HashSet<Integer> iSet = new HashSet<Integer>();	--> 원래 쓰면 이렇게
+		HashSet<Integer> iSet = new HashSet<Integer>();	//--> 섞어쓰기.
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+}
