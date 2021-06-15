87201062d49dfb0b3fc21230544854aee26f9fc4
wonho <wonho951@gmail.com>
2021-06-15 12:47:58 +0900

    2021.06.15 수업내용 List

--- src/com/javaex/ex04/PointApp.java
+++ src/com/javaex/ex04/PointApp.java
@@ -10,6 +10,7 @@ public class PointApp {
 		
 		Point p01 = new Point(2, 4);
 		Point p02 = new Point(3, 6);
+		Point p03 = new Point(4, 8);
 		
 		pList.add(p01);
 		System.out.println(pList.size());
@@ -18,8 +19,25 @@ public class PointApp {
 		pList.add(p02);	//위치 안정해주면 알아서 끝에 들어감
 		System.out.println(pList.size());
 		
+		pList.add(p03);
 		
-		Point p = pList.get(0);
+		
+		Point p = pList.get(1);
+		System.out.println(p.getX());
+		System.out.println(p.getY());
+		System.out.println(p.toString());
+		System.out.println(p);
+		
+		
+		
+		
+		
+		System.out.println("----------------------------------------");
+		for (int i = 0; i < pList.size(); i++) {
+			Point pp = pList.get(i);
+			System.out.println(pp.toString());
+		}
+		
 		
 		
 		
