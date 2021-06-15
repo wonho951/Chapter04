e7b1b353568f142c03c52ebe6fbadfbeaeb0790d
wonho <wonho951@gmail.com>
2021-06-15 11:01:52 +0900

    2021.06.15 수업내용 제네릭

---
+++ src/com/javaex/ex03/Circle.java
@@ -0,0 +1,37 @@
+package com.javaex.ex03;
+
+public class Circle {
+
+	//필드
+	private int radius;
+
+	
+	//생성자
+	public Circle() {
+		super();
+	}
+
+	public Circle(int radius) {
+		super();
+		this.radius = radius;
+	}
+
+	
+	//메소드 g/s
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+
+	
+	//메소드 - 일반
+	@Override
+	public String toString() {
+		return "Circle [radius=" + radius + "]";
+	}
+	
+	
+}
---
+++ src/com/javaex/ex03/MyListApp.java
@@ -0,0 +1,10 @@
+package com.javaex.ex03;
+
+public class MyListApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
