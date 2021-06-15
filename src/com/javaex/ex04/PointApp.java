8bd815e725ba9dc18f6bdc726b73a2c748365da2
wonho <wonho951@gmail.com>
2021-06-15 12:20:50 +0900

    2021.06.15 수업내용 List

--- src/com/javaex/ex04/PointApp.java
+++ src/com/javaex/ex04/PointApp.java
@@ -1,10 +1,12 @@
 package com.javaex.ex04;
 
+import java.util.ArrayList;
+
 public class PointApp {
 
 	public static void main(String[] args) {
 
-		
+		ArrayList<Point> pList= new ArrayList<Point>();	//Point만 들어오게함.
 		
 		Point p01 = new Point(2, 2);
 		
