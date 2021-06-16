2d6dbae3b0deec119d01017d9ff0604bcc148010
wonho <wonho951@gmail.com>
2021-06-16 11:23:49 +0900

    2021.06.16 수업내용 HashCode

--- src/com/javaex/ex07/Point.java
+++ src/com/javaex/ex07/Point.java
@@ -41,6 +41,9 @@ public class Point {
 		return "Point [x=" + x + ", y=" + y + "]";
 	}
 
+
+
+	
 	@Override
 	public boolean equals(Object obj) {
 		
@@ -58,9 +61,32 @@ public class Point {
 		return x+y;// + 13 ;	//--> 1차 hashcode에서 거르고 2차로 equals에서 거르는데 혹시 모르니까 수학적으로 뒤에 이상한 숫자 붙여서 더 거른다.(똑똑하누)
 	}
 	
-	
-	
-	
+	/*
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+	*/
 	
 	
 	
