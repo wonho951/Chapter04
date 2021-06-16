c3ed72632cba7e2ca3be53859d03d793c5aa7f4b
wonho <wonho951@gmail.com>
2021-06-16 11:56:29 +0900

    2021.06.16 수업내용 Map

--- src/com/javaex/ex08/PointApp.java
+++ src/com/javaex/ex08/PointApp.java
@@ -25,6 +25,22 @@ public class PointApp {
 		
 		System.out.println(pMap.size());
 		
+		System.out.println(pMap.toString());
+		
+		System.out.println(pMap.get("이효리").getX());	//꺼내올때 먼저 키로 불러서 꺼내옴.
+		
+		
+		Point p04 = new Point(100, 100);	//p04추가
+		
+		pMap.put("유재석", p04);	//키 = 유재석 --> 중복
+		
+		System.out.println(pMap.toString());	//유재석이 중복이기때문에 오류는 안나지만 출력시 03의 유재석이 수정되서 04의 유재석으로 출력됨.
+		
+		
+		
+		
+		
+		
 		
 	}
 
