0a20959cf8a751ee9099b19b472856ce867d95d6
wonho <wonho951@gmail.com>
2021-06-16 12:06:46 +0900

    2021.06.16 수업내용 Map

--- src/com/javaex/ex08/PointApp.java
+++ src/com/javaex/ex08/PointApp.java
@@ -1,7 +1,8 @@
 package com.javaex.ex08;
 
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class PointApp {
 
@@ -35,6 +36,52 @@ public class PointApp {
 		pMap.put("유재석", p04);	//키 = 유재석 --> 중복
 		
 		System.out.println(pMap.toString());	//유재석이 중복이기때문에 오류는 안나지만 출력시 03의 유재석이 수정되서 04의 유재석으로 출력됨.
+												//없는 줄 알고 쓰려고 하면 위험함. 잘 체크해야함.
+		
+		
+		//키 리스트를 만든다
+		//["정우성", "이효리", "유재석"] --> Map이 key들을 set으로 관리하고 있다.
+		//pMap.get(이름=키값) --> 얘를 계속 돌려서 꺼내와야함
+		
+		Set<String> kyes = pMap.keySet();
+		
+		
+		for (String key : kyes) {
+			System.out.println(pMap.get(key).getY());
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
